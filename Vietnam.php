8946904238005dc644bfb6418343b5c908820d59
xuannbgch17346 <xuannbgch17346@fpt.edu.vn>
2022-09-22 04:33:15 -0700

    3ok

---
+++ Vietnam.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vietnam</title>
+</head>
+<body>
+  <img" https://vcdn1-dulich.vnecdn.net/2021/07/16/8-1626444967.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=GfgGn4dNuKZexy1BGkAUNA=="">
+</body>
+</html>
