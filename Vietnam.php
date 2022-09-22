0bbab84eaed4eaaaecaf34418ae9b54156e8094d
xuannbgch17346 <xuannbgch17346@fpt.edu.vn>
2022-09-22 04:43:26 -0700

    5ok

--- Vietnam.php
+++ Vietnam.php
@@ -7,6 +7,8 @@
   <title>Vietnam</title>
 </head>
 <body>
+  <h1 style="color: red;">heroku</Head></h1>
+  <h2 class="text text-primary">i'm gosh</h2>
    <iframe width="560" height="315" src="https://www.youtube.com/embed/lxzOIev1Pec" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
   <img
   src= " https://vcdn1-dulich.vnecdn.net/2021/07/16/8-1626444967.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=GfgGn4dNuKZexy1BGkAUNA">
